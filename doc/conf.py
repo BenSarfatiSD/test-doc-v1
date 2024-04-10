8c80c56f235362012b2fb21a15c782b3c8bb8095
BenSarfatiSD <134265357+BenSarfatiSD@users.noreply.github.com>
2024-04-10 14:50:59 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,23 +9,36 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Example'
-copyright = 'workshop participant'
-author = 'workshop participant'
-release = '0.1'
-
+project = "Engine"
+copyright = """2024, Florian Denis, Louis Duverneuil, Bénédicte Rallet, Gabin
+ Rolland, Ben Sarfati"""
+author = """Florian Denis, Louis Duverneuil, Bénédicte Rallet, Gabin Rolland,
+ Ben Sarfati"""
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['myst_parser']
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.todo",
+    "sphinx_markdown_builder",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+# -- Path setup --------------------------------------------------------------
+sys.path.insert(0, os.path.abspath("../.."))
+
+# -- Display todos by setting to True ----------------------------------------
+todo_include_todos = True
